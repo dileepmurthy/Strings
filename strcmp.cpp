0790eb533b97e6da02b453ab9723d4b53ee09f75
dileepmurthy <dileepmurthy.tke@gmail.com>
2020-12-24 13:48:24 +0530

    Create strcmp.cpp

---
+++ strcmp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int len=0,i,j;
+    char str[10]="Dileep";
+    char cpy[20];
+    cout<<"String:"<<str<<"\n";
+    
+    for(int i=0;i<=str[i];i++)
+        len++;
+    cout<<"String Len:"<<len<<"\n";
+    
+    for(i=0;str[i]!='\0';i++)
+    {
+        cpy[i]=str[i];
+      
+    }
+      //rev[j]='\0';
+    cout<<"copied string is:"<<cpy;
+    
+}
